974b2a0a7b5ff23c2e5e85cbb1b88273eb5a42d9
Arios <ariosssmfm@gmail.com>
2021-03-04 10:43:53 +0200

    changing keylistener to keyadapter

--- src/Game.java
+++ src/Game.java
@@ -5,21 +5,21 @@ import javax.swing.WindowConstants;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Game extends JFrame implements KeyListener{
+public class Game extends JFrame {
 
-    private final int panel_height=20, panel_width=30, snake_starting_length=20,
-    r=KeyEvent.VK_RIGHT,l=KeyEvent.VK_LEFT,u=KeyEvent.VK_UP,d=KeyEvent.VK_DOWN;
-    private int last_key_pressed =r,last_key_performed =r;
+    private final int panel_height=20, panel_width=30, snake_starting_length=20, period=50,
+    right =KeyEvent.VK_RIGHT, left =KeyEvent.VK_LEFT, up =KeyEvent.VK_UP, down =KeyEvent.VK_DOWN;
+    private int last_key_pressed = right,last_key_performed = right;
 
-    private LinkedList<Pans>snake= new LinkedList<>();
-    private Pans[][] pin=new Pans[panel_height][panel_width];
+    private LinkedList<Square>snake=new LinkedList<>();
+    private Square[][] pin=new Square[panel_height][panel_width];
     private Random random_food=new Random();
     private Timer timer=new Timer();
     private JPanel main_panel;
@@ -27,7 +27,7 @@ public class Game extends JFrame implements KeyListener{
     private Game() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(new Dimension(panel_width*50, panel_height*50));
-        addKeyListener(this);
+        addKeyListener(new MyKeyAdapter());
         set_panels();
         create_snake();
         cook_food();
@@ -35,7 +35,7 @@ public class Game extends JFrame implements KeyListener{
             public void run(){
                 snake_action();
             }
-        },0,100);
+        },0,period);
     }
 
     public static void main(String[] args) {
@@ -46,7 +46,7 @@ public class Game extends JFrame implements KeyListener{
         add(main_panel=new JPanel(new GridLayout(panel_height, panel_width)));
         for(int i=0; i<panel_height; i++)
             for(int j=0; j<panel_width; j++)
-                main_panel.add(pin[i][j]=new Pans(i,j));
+                main_panel.add(pin[i][j]=new Square(i,j));
     }
 
 
@@ -69,26 +69,15 @@ public class Game extends JFrame implements KeyListener{
     }
 
 
-    public void keyTyped(KeyEvent ke) {}
-    public void keyReleased(KeyEvent ke) {}
-    public void keyPressed(KeyEvent ke){
-        int key_code = ke.getKeyCode();
-        if(key_code>=37&&key_code<=40)//key code range
-            if(Math.abs(key_code- last_key_performed)!=2)//not opposite directions
-                last_key_pressed =key_code;
-    }
-
-
     private void snake_action() {
-
         int i=snake.getFirst().i;
         int j=snake.getFirst().j;
 
         switch (last_key_pressed) {
-            case r -> j++;
-            case l -> j--;
-            case u -> i--;
-            case d -> i++;
+            case right -> j++;
+            case left -> j--;
+            case up -> i--;
+            case down -> i++;
         }
 
         i=(i+panel_height)%panel_height;
@@ -101,17 +90,17 @@ public class Game extends JFrame implements KeyListener{
     }
 
 
-    private void snake_moves(Pans moves_at){
-        Pans temp=snake.removeLast();
+    private void snake_moves(Square moves_at){
+        Square temp=snake.removeLast();
         temp.has_snake=false;
-        if(moves_at.has_snake){timer.cancel();return;}
+        if(moves_at.has_snake){timer.cancel();return;}//gameover
         temp.setBackground(null);
         snake.addFirst(moves_at);
         moves_at.setBackground(Color.green);
         moves_at.has_snake=true;
     }
 
-    private void snake_eats(Pans moves_at){
+    private void snake_eats(Square moves_at){
         snake.addFirst(moves_at);
         moves_at.setBackground(Color.green);
         moves_at.has_snake=true;
@@ -119,13 +108,23 @@ public class Game extends JFrame implements KeyListener{
         cook_food();
     }
 
-    private class Pans extends JPanel{
-        private final int i,j;
-        private boolean has_snake =false, has_food =false;
 
-        private Pans(int i, int j) {
+    private class Square extends JPanel{
+        private final int i,j;
+        private boolean has_snake=false, has_food=false;
+
+        private Square(int i, int j) {
             this.i = i;
             this.j = j;
+        }
+    }
+
+    private class MyKeyAdapter extends KeyAdapter{
+        public void keyPressed(KeyEvent e){
+            int key_code = e.getKeyCode();
+            if(key_code>=37&&key_code<=40)//key code range
+                if(Math.abs(key_code- last_key_performed)!=2)//not opposite directions
+                    last_key_pressed=key_code;
         }
     }
 
